fc9c9e34cda901d8e96f1e34696f840803fad363
Martijn de Vos <mdmartijndevos@gmail.com>
2015-04-18 11:42:27 +0200

    Initial Commit

---
+++ WWDC2015Submission/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WWDC2015Submission
+//
+//  Created by Martijn de Vos on 4/18/15.
+//  Copyright (c) 2015 CodeUp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
